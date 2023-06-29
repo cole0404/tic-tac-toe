484ae0d5d6313cbd566057306f3f2a5883bc145c
cole0404 <coletonboles2004@gmail.com>
2023-06-29 00:10:12 -0500

    One round in console almost working

---
+++ script.js
@@ -0,0 +1,36 @@
+const gameBoard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+  console.log(Cell());
+})();
+
+function Cell() {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue
+  };  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue
+  };
