10d5b3a24ad63e90285a8db4c00308456f10684d
cole0404 <coletonboles2004@gmail.com>
2023-06-29 00:11:26 -0500

    One round in console almost working

--- script.js
+++ script.js
@@ -9,28 +9,52 @@ const gameBoard = (function () {
       board[i].push(Cell());
     }
   }
-  console.log(Cell());
+
+  const getBoard = () => board;
+
+  const placeToken = (column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+
+    // If no cells make it through the filter,
+    // the move is invalid. Stop execution.
+    if (!availableCells.length) return;
+
+    // Otherwise, I have a valid cell, the last one in the filtered array
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+
+    gameBoard.printBoard();
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  // Here, we provide an interface for the rest of our
+  // application to interact with the board
+  return { getBoard, placeToken, printBoard };
 })();
 
 function Cell() {
   let value = 0;
 
+  // Accept a player's token to change the value of the cell
   const addToken = (player) => {
     value = player;
   };
 
+  // How we will retrieve the current value of this cell through closure
   const getValue = () => value;
 
   return {
     addToken,
-    getValue
-  };  const addToken = (player) => {
-    value = player;
+    getValue,
   };
+}
 
-  const getValue = () => value;
-
-  return {
-    addToken,
-    getValue
-  };
+gameBoard.printBoard();
